99d3bc668c426f220e270b84763a751384fc33c5
Gonzalo Lázaro <glazzam1005@g.educaand.es>
2024-03-04 09:22:28 +0100

    Arreglo tamaño de mensajes de error y label

--- js/login.js
+++ js/login.js
@@ -7,14 +7,14 @@ function validation(e) {
     let correcto = true;
     
     if (user === "" || !(user.length >= 8 && user.length <= 12)) {
-        document.getElementById('usuarioHelp').style.display = "block";
-        document.getElementById('usuario').style.borderColor = "yellow";
+        document.getElementById('userHelp').style.display = "block";
+        document.getElementById('user').style.borderColor = "yellow";
         correcto = false;
     }
     
     if (!(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$/.test(password))) {
-        alert("La contraseña debe contener entre 8 y 15 caracteres, una mayúscula, una minúscula y un número")
-        document.getElementById('passwordHelp').style.visibility = "block";
+        /* alert("La contraseña debe contener entre 8 y 15 caracteres, una mayúscula, una minúscula y un número") */
+        document.getElementById('passwordHelp').style.display = "block";
         document.getElementById('password').style.borderColor = "yellow";
         correcto = false;
     }
